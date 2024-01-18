518b8b3e9a0152273074f023471238557e196da6
Zachary Temkin <zachtemkin@gmail.com>
2024-01-17 23:40:02 -0500

    adds createPost script to aid in adding new posts

---
+++ createPost.js
@@ -0,0 +1,63 @@
+const fs = require("fs")
+const path = require("path")
+
+const getImagesFromDirectory = (title) => {
+  const imageExtensions = [".jpg", ".jpeg", ".png", ".gif"]
+  const imagesDirectory = path.join(
+    __dirname,
+    `src/posts/${title.toLowerCase().split(" ").join("-")}`
+  )
+  return fs
+    .readdirSync(imagesDirectory)
+    .filter((file) =>
+      imageExtensions.includes(path.extname(file).toLowerCase())
+    )
+    .map((file) => path.join(file))
+}
+
+const createMarkdownFile = (title, author, date, tags, images) => {
+  const postDirectory = path.join(
+    __dirname,
+    `src/posts/${title.toLowerCase().split(" ").join("-")}`
+  )
+  if (!fs.existsSync(postDirectory)) {
+    fs.mkdirSync(postDirectory)
+  }
+
+  const filename = `index.md`
+  const filePath = path.join(postDirectory, filename)
+
+  let frontMatterImages = images
+    .map((image) => `  - image: ${image}`)
+    .join("\n")
+
+  let frontMatterTags = tags
+    .split("/")
+    .map((tag) => `  - tag: ${tag}`)
+    .join("\n")
+
+  const content = `---
+contentType: post
+title: "${title}"
+date: "${date}"
+author: "${author}"
+categories:
+${frontMatterTags}
+imageGallery:
+${frontMatterImages}
+---
+`
+
+  fs.writeFileSync(filePath, content, "utf8")
+  console.log(`Post created: ${filePath}`)
+}
+
+const args = process.argv.slice(2)
+const title = args[0]
+const author = args[1]
+const date = args[2]
+const tags = args[3]
+
+const images = getImagesFromDirectory(title)
+
+createMarkdownFile(title, author, date, tags, images)
---
+++ src/posts/digital-paintings/index.md
@@ -0,0 +1,26 @@
+---
+contentType: post
+title: "digital-paintings"
+date: "2022-01-17"
+author: "Zach"
+categories:
+  - tag: digital painting
+  - tag: concept art
+imageGallery:
+  - image: balloon.jpg
+  - image: bluffs.jpg
+  - image: bot.jpg
+  - image: bridge.jpg
+  - image: cliffs.jpg
+  - image: frog.jpg
+  - image: horn.jpg
+  - image: hut.png
+  - image: mesa.jpg
+  - image: mill.jpg
+  - image: montana-1.jpg
+  - image: montana-2.jpg
+  - image: mountain.jpg
+  - image: poly.jpg
+  - image: swamp.jpg
+  - image: swirl.jpg
+---
